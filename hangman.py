7a93239bcf84b6fd738cb23535e4f1ab9f2f4e27
Erokhar <44982649+Erokhar@users.noreply.github.com>
2019-07-04 11:03:08 +0100

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -8,8 +8,8 @@
 
 
 
-#imports
-from IPython.display import clear_output
+#from IPython.display import clear_output
+#i can't use ipython currently so i commented it for now
 import random
 
 class game():
@@ -26,7 +26,7 @@ class game():
             print('Select a category to play from.')
 
         #selecting category input
-            question = input('Famous People or Movies')
+            question = input('Famous People or Movies: ')
             if question.lower() == 'famous people':
                 print('inside famous people')
                 self.selectWord()
@@ -41,15 +41,64 @@ class game():
 #selects word for the category 'famous people' based on the number sent
     def selectWord(self):
         wordbank = ['Miley Cyrus', 'Tom Delonge', 'Bill Murray', 'Morgan Freeman','Keith Richards']
-        print(wordbank[random.randint(0,4)])
-
-
+        selected=wordbank[random.randint(0,4)]
+        self.backgroundwork(selected)
+        
 
 #selects word for the category 'tv shows' based on the number sent
     def selectWordTwo(self):
         wordbank = ['Interstellar', 'Planet of the Apes', 'The Departed', 'Wolf of Wall Street','Source Code']
-        print(wordbank[random.randint(0,4)])
+        selected=wordbank[random.randint(0,4)]
+        self.backgroundwork(selected)
+
+#Modifications by user Erokhar
+    def backgroundwork(self,selected):
+        inputted=""
+        currentLine=[]
+        currentLineString=""
+        s=""
+        lives=5
+#setting the list for use
+        for x in selected:
+            if(x!=" "):
+                currentLine.append("_")
+            else:
+                currentLine.append(" ")
+        for x in currentLine:
+            s+=x
+        print(s)
+
+        while currentLineString.lower()!=selected.lower():
+            found=0
+            currentLineString=""
+            inputted=input("input letter: ")
+#iterating around the given string and checking for the input letter inside
+            for x in range(len(selected)):
+                if selected[x].lower()==inputted.lower():
+                    currentLine[x]=inputted
+                    found=1
+            if found!=1:
+                if lives>1:
+                    lives-=1
+
+                    print("Wrong! lives left: ", lives)
+                else:
+                    print("No more lives. GAME OVER!")
+                    print("The answer was: ", selected)
+                    return 0
+#Making a space....well space in the list
+            if selected[x]==" ":
+                currentLine[x]=" "
+#setting the final string that will be compared with the original selected word
+            for x in currentLine:
+                currentLineString+=x
+            print(currentLineString)
+                
+        print("Congratulations! The answer was: ", currentLineString)
+
+
 
 
 g = game()
 g.selectCategory()
+
